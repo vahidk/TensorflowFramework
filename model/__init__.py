771335f7094bf00d3ca47aef8381803573459efa
Vahid Kazemi <vkazemi@snapchat.com>
2018-12-04 09:57:23 -0800

    Remove excluded packages.

--- model/__init__.py
+++ model/__init__.py
@@ -1,7 +1,3 @@
 from model import alexnet
 from model import allconv
-from model import audio_transformer
-from model import resnet
-from model import autoencoder
-from model import regressor
 from model import resnet
